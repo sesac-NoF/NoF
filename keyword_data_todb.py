4e534dc9971d4557b662850079df1fae0d77d1b8
jungssg <younwjdtjr@naver.com>
2024-10-18 11:05:41 +0900

    근로,사기,상속,상해 목록/본문 데이터 데이터베이스저장

---
+++ keyword_data_todb.py
@@ -0,0 +1,41 @@
+# db로 저장
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import LONGTEXT, TEXT
+from db_info import user, password, host, port, database
+
+####pandas dataframe에서는 불러올때 타입을 기본str로 불러와서 긴내용의 데이터삽입을 하려면 dtype을 설정해줘야함####
+dtype = {
+    '판례내용' : LONGTEXT,
+    '판시사항': TEXT,
+    '판결요지': TEXT,
+    '참조조문': TEXT,
+    '참조판례': TEXT,
+}
+
+# DB정보
+user = user
+password = password
+host = host
+port = port
+database = database
+
+# CSV 파일 로드
+df_injury = pd.read_csv('data/상해_본문2.csv')
+df_fraud = pd.read_csv('data/사기_본문2.csv')
+df_inheritance = pd.read_csv('data/상속_본문2.csv')
+df_labor = pd.read_csv('data/근로_본문2.csv')
+
+# MySQL 데이터베이스 연결 설정
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
+
+# 데이터프레임을 MySQL 테이블에 저장
+df_injury.to_sql(name='injury_content', con=engine, if_exists='replace', index=False, dtype=dtype)
+df_fraud.to_sql('fraud_content', con=engine, if_exists='replace', index=False, dtype=dtype)
+df_inheritance.to_sql('inheritance_content', con=engine, if_exists='replace', index=False, dtype=dtype)
+df_labor.to_sql('labor_content', con=engine, if_exists='replace', index=False, dtype=dtype)
+
+# 연결 닫기
+engine.dispose()
+
+print("데이터베이스 저장 완료")
--- mysql_db.py
+++ mysql_db.py
@@ -5,11 +5,6 @@ from sqlalchemy import create_engine
 from db_info import user, password, host, port, database
 
 df = pd.read_csv('data/lawSearch_total.csv')
-df_injury = pd.read_csv('data/상해_목록.csv')
-df_fraud = pd.read_csv('data/사기_목록.csv')
-df_inheritance = pd.read_csv('data/상속_목록.csv')
-df_labor = pd.read_csv('data/근로_목록.csv')
-
 
 user = user
 password = password
@@ -21,10 +16,7 @@ database = database
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', echo=False)
 
 # 데이터프레임을 MySQL 테이블에 저장 
-df_injury.to_sql(name='injury_list', con=engine, if_exists='replace', index=False)
-df_fraud.to_sql(name='fraud_list', con=engine, if_exists='replace', index=False)
-df_inheritance.to_sql(name='inheritance_list', con=engine, if_exists='replace', index=False)
-df_labor.to_sql(name='labor_list', con=engine, if_exists='replace', index=False)
+df.to_sql(name='injury_list', con=engine, if_exists='replace', index=False)
 
 # 연결 닫기
 engine.dispose()
