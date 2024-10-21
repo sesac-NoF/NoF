aedcc5ad7a1e017487b3d668b9194dfc636884ac
jungssg <131239981+jungssg@users.noreply.github.com>
2024-10-21 16:21:19 +0900

    4 law search (#12)
    API사용 판례목록, 판례본문, 키워드별 판례 데이터

--- lawService_js.py
+++ lawService_js.py
@@ -2,104 +2,47 @@ import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
 
-
-df = pd.read_csv('data/df_data.csv')
+df = pd.read_csv('data/lawSearch_js.csv')
 
 OC = 'younwjdtjr'
 target = 'prec'
 url = 'http://www.law.go.kr/DRF/lawService.do?'
 data = []
 
-# for i in range(1,df['판례일련번호'].value_counts().sum()+1):
-#     params = {
-#         'OC' : OC,
-#         'target' : target,
-#         'type' : 'XML',
-#         'ID' : df['판례일련번호'].iloc[i-1]
-#     }
+for i in range(1,df['판례일련번호'].value_counts().sum()+1):
+    params = {
+        'OC' : OC,
+        'target' : target,
+        'type' : 'XML',
+        'ID' : df['판례일련번호'].iloc[i-1]
+    }
+    response = requests.get(url, params=params, timeout=10)
 
-
-params = {
-    'OC' : OC,
-    'target' : target,
-    'type' : 'XML',
-    'ID' : df['판례일련번호'].iloc[1]
-}
-response = requests.get(url, params=params)
-
-if response.status_code == 200:
-    root = ET.fromstring(response.content)
-    CASE_NUM = root.find('.//사건번호')
-    JUD = root.find('.//선고')
-    JUD_TYPE = root.find('.//판결유형')
-    ISSUE = root.find('.//판시사항')
-    SUMMARY_JUD = root.find('.//판결요지')
-    REF_STAT = root.find('.//참조조문')
-    REF_CASE = root.find('.//참조판례')
-    CASE_CONTENT = root.find('.//판례내용')
-    print(CASE_CONTENT.text)
-    for 판례내용 in root.findall('.//판례내용'):
-        content = 판례내용.text
-        # '【주 문】' 이후의 텍스트 추출
-        start_index = content.find('【피 고 인】')
-    if start_index != -1:
-        # '【이 유】' 전까지 추출
-        end_index = content.find('【상 고 인】', start_index)
-        if end_index != -1:
-            order_text = content[start_index:end_index].strip()
-            print(order_text)
-        else:
-            order_text = content[start_index:].strip()  # '【이 유】'이 없는 경우
-            print(order_text)
-    for 판례내용 in root.findall('.//판례내용'):
-        content = 판례내용.text
-        # '【주 문】' 이후의 텍스트 추출
-        start_index = content.find('【상 고 인】')
-    if start_index != -1:
-        # '【이 유】' 전까지 추출
-        end_index = content.find('【변 호 인】', start_index)
-        if end_index != -1:
-            order_text = content[start_index:end_index].strip()
-            print(order_text)
-        else:
-            order_text = content[start_index:].strip()  # '【이 유】'이 없는 경우
-            print(order_text)
-    for 판례내용 in root.findall('.//판례내용'):
-        content = 판례내용.text
-        # '【주 문】' 이후의 텍스트 추출
-        start_index = content.find('【변 호 인】')
-    if start_index != -1:
-        # '【이 유】' 전까지 추출
-        end_index = content.find('【원심판결】', start_index)
-        if end_index != -1:
-            order_text = content[start_index:end_index].strip()
-            print(order_text)
-        else:
-            order_text = content[start_index:].strip()  # '【이 유】'이 없는 경우
-            print(order_text)
-    for 판례내용 in root.findall('.//판례내용'):
-        content = 판례내용.text
-        # '【주 문】' 이후의 텍스트 추출
-        start_index = content.find('【원심판결】')
-    if start_index != -1:
-        # '【이 유】' 전까지 추출
-        end_index = content.find('【주    문】', start_index)
-        if end_index != -1:
-            order_text = content[start_index:end_index].strip()
-            print(order_text)
-        else:
-            order_text = content[start_index:].strip()  # '【이 유】'이 없는 경우
-            print(order_text)
-    for 판례내용 in root.findall('.//판례내용'):
-        content = 판례내용.text
-        # '【주 문】' 이후의 텍스트 추출
-        start_index = content.find('【주    문】')
-    if start_index != -1:
-        # '【이 유】' 전까지 추출
-        end_index = content.find('【이    유】', start_index)
-        if end_index != -1:
-            order_text = content[start_index:end_index].strip()
-            print(order_text)
-        else:
-            order_text = content[start_index:].strip()  # '【이 유】'이 없는 경우
-            print(order_text)
+    if response.status_code == 200:
+        root = ET.fromstring(response.content)
+        CASE_NUM = root.find('.//사건번호')
+        JUD = root.find('.//선고')
+        JUD_TYPE = root.find('.//판결유형')
+        ISSUE = root.find('.//판시사항')
+        SUMMARY_JUD = root.find('.//판결요지')
+        REF_STAT = root.find('.//참조조문')
+        REF_CASE = root.find('.//참조판례')
+        CASE_CONTENT = root.find('.//판례내용')
+      
+        data.append([
+                        df['판례일련번호'].iloc[i-1],
+                        CASE_NUM.text if CASE_NUM is not None else None,
+                        JUD.text if JUD is not None else None,
+                        JUD_TYPE.text if JUD_TYPE is not None else None,
+                        ISSUE.text if ISSUE is not None else None,
+                        SUMMARY_JUD.text if SUMMARY_JUD is not None else None,
+                        REF_STAT.text if REF_STAT is not None else None,
+                        REF_CASE.text if REF_CASE is not None else None,
+                        CASE_CONTENT.text if CASE_CONTENT is not None else None
+                    ])
+    else:
+        print(i,'error')
+    print(i)
+    
+df2 = pd.DataFrame(data=data, columns=['판례정보일련번호','사건번호','선고','판결유형','판시사항','판결요지','참조조문','참조판례','판례내용'])
+df2.to_csv('data/lawService_js.csv', index=False)
