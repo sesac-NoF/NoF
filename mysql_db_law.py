2e8dc2e9c00bc3bfe6b6a4b81156ddaf9651d083
jungssg <younwjdtjr@naver.com>
2024-10-21 13:37:01 +0900

    법령 목록,본문/ 데이터베이스에서 데이터꺼내오기

--- mysql_db_law.py
+++ mysql_db_law.py
@@ -5,15 +5,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.dialects.mysql import LONGTEXT, TEXT
 from db_info import user, password, host, port, database
 
-dtype = {
-    '판례내용' : LONGTEXT,
-    '판시사항': TEXT,
-    '판결요지': TEXT,
-    '참조조문': TEXT,
-    '참조판례': TEXT,
-}
+# dtype = {
+#     '법령일련번호' : INT,
+#     '현행연혁코드': TEXT,
+#     '벌령명한글': TEXT,
+#     '법령약칭명': TEXT,
+#     '법령ID': TEXT,
+#     '공포일자': TEXT,
+#     '공포번호': TEXT,
+#     '제개정구분명': TEXT,
+#     '소관부처코드': TEXT,
+#     '소관부처명': TEXT,
+#     '법령구분명': TEXT,
+#     '시행일자': TEXT,
+#     '법령상세링크': TEXT,
+# }
 
-df = pd.read_csv('data/lawService_total.csv')
+df = pd.read_csv('data/laws_original.csv')
 
 user = user
 password = password
@@ -26,7 +34,7 @@ database = database
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', echo=False)
 
 # 데이터프레임을 MySQL 테이블에 저장 
-df.to_sql(name='law_service_table', con=engine, if_exists='replace', index=False, dtype=dtype)
+df.to_sql(name='law_content', con=engine, if_exists='replace', index=False)
 
 # 연결 닫기
 engine.dispose()
