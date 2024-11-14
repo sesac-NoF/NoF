6e19abe306a9b1b29e2e47a080c71d1f0552cf45
jungssg <younwjdtjr@naver.com>
2024-11-14 11:15:58 +0900

    lawService 코드수정

--- lawService_js.py
+++ lawService_js.py
@@ -2,24 +2,28 @@ import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
 
-df = pd.read_csv('data/lawSearch_js.csv')
-
+df = pd.read_csv('data/law_list22.csv', index_col=False)
+print(df['판례일련번호'].value_counts().sum())
+print(len(df))
+print(df[['판례일련번호','판례상세링크']].head())
+print(df[['판례일련번호','판례상세링크']].tail())
 OC = 'younwjdtjr'
 target = 'prec'
 url = 'http://www.law.go.kr/DRF/lawService.do?'
 data = []
 
-for i in range(1,df['판례일련번호'].value_counts().sum()+1):
+for i in range(len(df)):
     params = {
         'OC' : OC,
         'target' : target,
         'type' : 'XML',
-        'ID' : df['판례일련번호'].iloc[i-1]
+        'ID' : df['판례일련번호'].iloc[i]
     }
     response = requests.get(url, params=params, timeout=10)
 
     if response.status_code == 200:
         root = ET.fromstring(response.content)
+        ORDER_NUM = root.find('.//판례정보일련번호')
         CASE_NUM = root.find('.//사건번호')
         JUD = root.find('.//선고')
         JUD_TYPE = root.find('.//판결유형')
@@ -30,7 +34,7 @@ for i in range(1,df['판례일련번호'].value_counts().sum()+1):
         CASE_CONTENT = root.find('.//판례내용')
       
         data.append([
-                        df['판례일련번호'].iloc[i-1],
+                        ORDER_NUM.text,
                         CASE_NUM.text if CASE_NUM is not None else None,
                         JUD.text if JUD is not None else None,
                         JUD_TYPE.text if JUD_TYPE is not None else None,
@@ -40,9 +44,8 @@ for i in range(1,df['판례일련번호'].value_counts().sum()+1):
                         REF_CASE.text if REF_CASE is not None else None,
                         CASE_CONTENT.text if CASE_CONTENT is not None else None
                     ])
+        print(i, df['판례일련번호'].iloc[i], ORDER_NUM.text)
     else:
         print(i,'error')
-    print(i)
-    
 df2 = pd.DataFrame(data=data, columns=['판례정보일련번호','사건번호','선고','판결유형','판시사항','판결요지','참조조문','참조판례','판례내용'])
-df2.to_csv('data/lawService_js.csv', index=False)
+# df2.to_csv('data/lawService_js.csv', index=False)
